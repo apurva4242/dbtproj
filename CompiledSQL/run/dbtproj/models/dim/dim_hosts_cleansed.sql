5909b4deee5f8c124c6501ca45a1926e5c9f404d
apurva4242 <139752238+apurva4242@users.noreply.github.com>
2023-07-19 00:58:50 +0530

    Added Course work till Lesson 41 - yml Changes for materialized: View & materialized: Table

---
+++ CompiledSQL/run/dbtproj/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,23 @@
+
+  
+    
+
+        create or replace transient table AIRBNB.DEV.dim_hosts_cleansed
+         as
+        (WITH src_hosts AS (
+ SELECT
+ *
+ FROM
+ AIRBNB.DEV.src_hosts
+)
+SELECT
+ host_id,
+NVL(host_name,'Anonymous') As HOST_NAME,
+ is_superhost,
+ created_at,
+ updated_at
+FROM
+ src_hosts
+        );
+      
+  
---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,14 @@
+WITH src_hosts AS (
+ SELECT
+ *
+ FROM
+ {{ref('src_hosts')}}
+)
+SELECT
+ host_id,
+NVL(host_name,'Anonymous') As HOST_NAME,
+ is_superhost,
+ created_at,
+ updated_at
+FROM
+ src_hosts
