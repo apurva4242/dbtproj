0caf39fc62da31e4233cd6b0aea8c00e0190ec77
apurva4242 <139752238+apurva4242@users.noreply.github.com>
2023-07-19 23:53:09 +0530

    69-Added a Post Hook to Grant Role in dbtproj.yml

--- CompiledSQL/run/dbtproj/models/fct/fct_reviews.sql
+++ CompiledSQL/run/dbtproj/models/fct/fct_reviews.sql
@@ -1,35 +1,11 @@
-
+-- back compat for old kwarg name
   
+  begin;
     
 
-        create or replace transient table AIRBNB.DEV.fct_reviews
-         as
+        insert into AIRBNB.DEV.fct_reviews ("REVIEW_ID", "LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT")
         (
-WITH  __dbt__cte__src_reviews as (
-WITH RAW_REVIEWS AS (
- SELECT
- *
- FROM
-AIRBNB.raw.raw_reviews
-)
-SELECT
-LISTING_ID,
-DATE as REVIEW_DATE,
-REVIEWER_NAME,
-COMMENTS AS REVIEW_TEXT,
-SENTIMENT AS REVIEW_SENTIMENT
-FROM
-RAW_REVIEWS
-),src_reviews AS (
- SELECT * FROM __dbt__cte__src_reviews
-)
-SELECT
- md5(cast(coalesce(cast(listing_id as TEXT), '') || '-' || coalesce(cast(review_date as TEXT), '') || '-' || coalesce(cast(reviewer_name as TEXT), '') || '-' || coalesce(cast(review_text as TEXT), '') as TEXT))
- AS review_id,
- *
- FROM src_reviews
-WHERE review_text is not null
-
+            select "REVIEW_ID", "LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT"
+            from AIRBNB.DEV.fct_reviews__dbt_tmp
         );
-      
-  
+    commit;
